b9b4e8a4c8484d6024f7feb16dd63bcd84b3d888
CesarLazaro <48570814+CesarLazaro@users.noreply.github.com>
2019-03-21 10:51:37 -0600

    Add files via upload

---
+++ Metodos de extencion/Metodos de extencion/Extencion}.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos_de_extencion
+{
+    public static class Extencion_
+    {
+        public static bool Digito(this String str)
+        {
+            List<char> tempStr = str.ToList();
+            tempStr.RemoveAll(c => c == ' ');
+            bool r = tempStr.All(c => Char.IsDigit(c));
+            return r;
+
+        }
+        public static bool Letra(this String str)
+        {
+            List<char> tempStr = str.ToList();
+            tempStr.RemoveAll(c => c == ' ');
+            bool r = tempStr.All(c => Char.IsLetter(c));
+            return r;
+
+        }
+        public static bool LetraoDigito(this String str)
+        {
+            List<char> tempStr = str.ToList();
+            tempStr.RemoveAll(c => c == ' ');
+            bool r = tempStr.All(c => Char.IsLetterOrDigit(c));
+            return r;
+
+        }
+
+
+    }
+}
---
+++ Metodos de extencion/Metodos de extencion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos_de_extencion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ejemplo = "Ces(///4ar";
+            if(ejemplo.Digito())
+            {
+                Console.WriteLine("Es digito");
+            }
+            if (ejemplo.Letra())
+            {
+                Console.WriteLine("Es letra");
+            }
+            if (ejemplo.LetraoDigito())
+            {
+                Console.WriteLine("Es letras con digitos");
+            }
+            Console.ReadKey();
+        }
+    }
+}
